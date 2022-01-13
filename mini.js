4cb77b60d1abd4bd099fe1c1bb049a6eb91bc927
marcelojoc <marcelojoc@gmail.com>
2022-01-13 15:29:27 -0300

    iniciando un proxyjs

--- mini.js
+++ mini.js
@@ -4,7 +4,25 @@ class PlatziReactive {
         data() => { ... }
     */
     constructor({ data }) {
-      this.origen = data();
+        this.origen = data();
+
+        // Destino
+        /*
+        ES6 pone a nuestra disposición una nueva y poderosa herramienta que permite aprovechar 
+        algunos aspectos de la metaprogramación que hasta ahora no estaban disponibles con Javascript: Proxy(). 
+        Va a permitirnos interceptar las operaciones con objetos y sus propiedades de tal forma que podemos 
+        redefinir el comportamiento para cada una de estas acciones.
+        */        
+        this.$data = new Proxy(this.origen, {
+            get(target, name) {
+            if (name in target) {
+                return target[name];
+            }
+            console.warn("La propiedad", name, "no existe");
+            return "";
+            },
+            set() {}
+        });
     }
   
     mount() {
